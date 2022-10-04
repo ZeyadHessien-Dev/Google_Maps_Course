c1afe9187aa0208eecd916e1a568e0c9f22ebf04
ZeyaD HessieN <zeyad.hessien1123@gmail.com>
2022-10-04 14:17:50 +0200

    1 - Flutter Google Maps - Creating your first flutter Google maps app

---
+++ lib/page/Home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Google Map'),
+      ),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            31.205753,
+            29.924526,
+          ),
+          zoom: 19,
+        ),
+      ),
+    );
+  }
+}
